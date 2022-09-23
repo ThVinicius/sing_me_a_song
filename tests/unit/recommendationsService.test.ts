8f1b46b14b52868bbaf476b06a66e84505b26b10
Vinicius Pacheco <vinicioss9955@gmail.com>
2022-09-23 12:07:35 -0300

    unit tests of downvote function completed

--- tests/unit/recommendationsService.test.ts
+++ tests/unit/recommendationsService.test.ts
@@ -69,3 +69,61 @@ describe('testando a função upvote', () => {
     expect(promise).rejects.toEqual({ type: 'not_found', message: '' })
   })
 })
+
+describe('testando a função downvote', () => {
+  it('testando com id existente', async () => {
+    const id = 1
+
+    const findRecommendation = findRecommendationFactory()
+
+    jest
+      .spyOn(recommendationRepository, 'find')
+      .mockResolvedValueOnce(findRecommendation)
+
+    jest
+      .spyOn(recommendationRepository, 'updateScore')
+      .mockResolvedValueOnce(findRecommendation)
+
+    await recommendationService.downvote(id)
+
+    expect(recommendationRepository.find).toBeCalled()
+
+    expect(recommendationRepository.updateScore).toBeCalled()
+  })
+
+  it('testando com id inexistente', () => {
+    const id = 1
+
+    jest.spyOn(recommendationRepository, 'find').mockResolvedValueOnce(null)
+
+    const promise = recommendationService.downvote(id)
+
+    expect(promise).rejects.toEqual({ type: 'not_found', message: '' })
+  })
+
+  it('testando com score abaixo de -5', async () => {
+    const id = 1
+
+    const findRecommendation = findRecommendationFactory()
+
+    findRecommendation.score = -6
+
+    jest
+      .spyOn(recommendationRepository, 'find')
+      .mockResolvedValueOnce(findRecommendation)
+
+    jest
+      .spyOn(recommendationRepository, 'updateScore')
+      .mockResolvedValueOnce(findRecommendation)
+
+    jest.spyOn(recommendationRepository, 'remove').mockResolvedValueOnce(null)
+
+    await recommendationService.downvote(id)
+
+    expect(recommendationRepository.find).toBeCalled()
+
+    expect(recommendationRepository.updateScore).toBeCalled()
+
+    expect(recommendationRepository.remove).toBeCalled()
+  })
+})
